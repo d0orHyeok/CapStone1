fdd38b3415ff384aede6e37e116bad432db6c1e8
d0oRHyeok <d0oR.hyeok@gmail.com>
2020-04-21 21:18:20 +0900

    전체 리뷰가 드러나도록 스크롤 후 크롤링 완료 class 생성 작업 전

--- Crawler/crawler.py
+++ Crawler/crawler.py
@@ -1,47 +1,62 @@
 import time
+import re
 import pyautogui
 from selenium import webdriver
 from bs4 import BeautifulSoup
+
+
+def scroll_page():
+    ### 자동 스크롤링
+    SCROLL_PAUSE_TIME = 1
+
+    # Get scroll height
+    last_height = driver.execute_script("return document.body.scrollHeight")
+
+    while True:
+        # Scroll down to bottom
+        driver.execute_script("window.scrollTo(0, document.body.scrollHeight);")
+        # 추가 로드를 위한 마우스 스크롤 직접 동작
+        pyautogui.scroll(-100)
+        pyautogui.scroll(100)
+
+        # Wait to load page
+        time.sleep(SCROLL_PAUSE_TIME)
+
+        # Calculate new scroll height and compare with last scroll height
+        new_height = driver.execute_script("return document.body.scrollHeight")
+        if new_height == last_height:
+            break
+        last_height = new_height
+
+def get_review_num():
+    totalReview = driver.find_element_by_class_name('_2HRMT3')  # 후기 (N)개 부분 추출
+    numTexts = re.findall("\d+", totalReview.text)  # 텍스트중 숫자 추출
+
+    #정수변환
+    if len(numTexts) == 1:              # 리뷰가 1000개 미만일 경우
+        numOfReview = int(numTexts[0])
+    else:
+        numOfReview = int(numTexts[0]) * 1000 + int(numTexts[1])
+
+    return numOfReview
+
 
 #driver 경로 설정
 driver = webdriver.Chrome('C:/Program Files (x86)/Google/ChromeDriver/chromedriver')
 driver.implicitly_wait(3)
 
 #수집 진행 url 접속
-driver.get('https://www.yanolja.com/hotel/3012531/reviews')
+driver.get('https://www.yanolja.com/hotel/3020210/reviews')
 
-
-### 자동 스크롤링 ###
-SCROLL_PAUSE_TIME = 1
-
-# Get scroll height
-last_height = driver.execute_script("return document.body.scrollHeight")
+numOfReview = get_review_num()
+print(numOfReview)
 
 while True:
-    # Scroll down to bottom
-    driver.execute_script("window.scrollTo(0, document.body.scrollHeight);")
-    # 추가 로드를 위한 마우스 스크롤 직접 동작
-    pyautogui.scroll(-100)
-    pyautogui.scroll(100)
+    scroll_page()
 
-    # Wait to load page
-    time.sleep(SCROLL_PAUSE_TIME)
-
-    # Calculate new scroll height and compare with last scroll height
-    new_height = driver.execute_script("return document.body.scrollHeight")
-    if new_height == last_height:
+    reviews = driver.find_elements_by_class_name('_2z8CVX')
+    if numOfReview == len(reviews) :
         break
-    last_height = new_height
 
-
-
-html = driver.page_source
-soup = BeautifulSoup(html, 'html.parser')
-
-#리뷰 선택
-reviews = soup.select('div._2z8CVX')
-
-print(len(reviews))
-
-#or review in reviews:
- #   print(review.text.strip())
+for review in reviews:
+    print(len(review))
